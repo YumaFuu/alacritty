cc0a75b93ae805e12fe97160deb796b1096764f7
Yuma Ishikawa <yuma.fuu05@gmail.com>
2020-09-30 21:52:10 +0900

    [fix] bin

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 path="$HOME/.config/alacrity"
 mkdir -p $path
-curl https://raw.githubusercontent.com/YumaFuu/alacrity/master/alacrity.yml > $path/alacrity.yml
+curl https://raw.githubusercontent.com/YumaFuu/alacrity/master/alacritty.yml > $path/alacrity.yml
 
 echo done
